a51179ee4852b33383f804a02ec2f4ab6abbb59d
Suphakorn <suphakorn04413@gmail.com>
2025-03-12 22:22:52 +0700

    fix capital in user model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     user_id: { 
         type: Number, 
         required: true, 
