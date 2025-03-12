9512ec1a3d58086c9b2d580277b683522e966f7e
TenJit <tenjitrattanakul@gmail.com>
2025-03-12 22:20:09 +0700

    fix yan template name

--- Backend/models/YanTemplate.js
+++ Backend/models/YanTemplate.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
 const yanTemplateSchema = new mongoose.Schema({
-  yan_template_id: { type: Number, required: true },
-  yan_template: [{ type : Number, enum: [0,1,2,3]}],
-  yan_cateogry: [{type : Number}],
-  background: { type: String, required: true},
-  export_count: { type: Number , default: 0}
+  yan_template_id: { 
+    type: Number, 
+    required: true 
+  },
+  yan_template: [{ 
+    type : Number, 
+    enum: [0,1,2,3]
+  }],
+  yan_cateogry: [{
+    type : Number
+  }],
+  background: { 
+    type: String, 
+    required: true
+  },
+  export_count: { 
+    type: Number, 
+    default: 0
+  }
 });
 
-module.exports = mongoose.model('ํYanTemplate', yanTemplateSchema);
+module.exports = mongoose.model('YanTemplate', yanTemplateSchema);
--- Backend/models/YanTemplateImage.js
+++ Backend/models/YanTemplateImage.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const yanTemplateImageSchema = new mongoose.Schema({
-  yan_template_image_id: { type: Number, required: true },
-  yan_category: [{ type : Number }],
-  yan_level: { type: Number, enum: [0,1,2,3], required: true },
-  yan_image_base64: { type: String },
+  yan_template_image_id: { 
+    type: Number, 
+    required: true 
+  },
+  yan_category: [{ 
+    type : Number 
+  }],
+  yan_level: { 
+    type: Number, 
+    enum: [0,1,2,3], 
+    required: true },
+    yan_image_base64: { 
+      type: String 
+    },
 });
 
-module.exports = mongoose.model('ํYanTemplateImage', yanTemplateImageSchema);
+module.exports = mongoose.model('YanTemplateImage', yanTemplateImageSchema);
