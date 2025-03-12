43fa87417f9b1151f550f5063cde357c0ba41fdd
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-03-12 22:06:34 +0700

    Add MongoDB Database Connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 dotenv.config({path:'./config/config.env'})
+
+connectDB()
 
 const app = express()
 
@@ -11,4 +14,9 @@ app.get('/', (req , res) => {
 
 const PORT = process.env.PORT || 1234
 
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port', PORT))
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port', PORT))
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error ${err.message}`)
+    server.close(() => process.exit(1))
+})
