09901502812d5019a545baac2623414789a9bb19
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-03-13 18:47:29 +0700

    Add Vish Route

--- Backend/routes/vish.js
+++ Backend/routes/vish.js
@@ -2,9 +2,10 @@ const express = require('express')
 const {getVishs, createVishTimeStamp, createVish}= require('../controllers/vishs')
 
 const router = express.Router()
+const { protect } = require('../middleware/user');
 
 router.route('/').get(getVishs)
-router.route('/createvish').post(createVish)
+router.route('/createvish').post(protect, createVish)
 router.route('/createvishtimestamp').post(createVishTimeStamp)
 
 module.exports = router
