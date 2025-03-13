c409f68042328568f5228ddc76cfbbe373d9b285
TenJit <tenjitrattanakul@gmail.com>
2025-03-13 21:27:18 +0700

    add yan template controller

--- Backend/controllers/YanTemplate.js
+++ Backend/controllers/YanTemplate.js
@@ -29,3 +29,74 @@ exports.createYanTemplate = async (req, res, next) => {
         })
     }
 };
+
+// @desc    Get all YanTemplates
+// @route   GET /api/yan/template
+// @access  Public
+exports.getYanTemplates = async (req, res, next) => {
+    try {
+        const page = parseInt(req.query.page, 10) || 1;
+        const { category, sort, asc , color } = req.query;
+        const limit = 10;
+        const skip = (page - 1) * limit;
+
+        let query = {};
+        if (category) query.yan_category = category;
+
+        if (color) query.background_color = color;
+
+        let sortOption = {};
+        if (sort) {
+            sortOption[sort] = asc === "true" ? 1 : -1;
+        }
+
+        const totalDocuments = await YanTemplate.countDocuments(query);
+        const maxPage = Math.ceil(totalDocuments / limit);
+
+        const yanTemplate = await YanTemplate.find(query)
+            .sort(sortOption)
+            .limit(limit)
+            .skip(skip);
+
+        res.status(200).json({
+            success: true,
+            count: yanTemplate.length,
+            lastpage : maxPage,
+            data: yanTemplate,
+        })
+    }
+    catch (error) {
+        res.status(400).json({
+            success: false,
+            msg: error.message,
+        })
+    }
+};
+
+// @desc    Get a YanTemplate
+// @route   GET /api/yan/template/:id
+// @access  Public
+exports.getYanTemplate = async (req, res, next) => {
+    try {
+        const yanTemplate = await YanTemplate.findById(req.params.id);  
+
+        if (!yanTemplate) {
+            return res.status(404).json({
+                success: false,
+                msg: 'Yan Template not found'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: yanTemplate,
+        })
+    }
+    catch (error) {
+        res.status(400).json({
+            success: false,
+            msg: error.message,
+        })
+    }
+}
+
--- Backend/routes/YanTemplate.js
+++ Backend/routes/YanTemplate.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { createYanTemplate } = require('../controllers/YanTemplate');
+const { createYanTemplate , getYanTemplates , getYanTemplate } = require('../controllers/YanTemplate');
 const router = express.Router();
 
 router.route('/').post( createYanTemplate);
+router.route('/').get( getYanTemplates);
+router.route('/:id').get( getYanTemplate);
 
 module.exports = router;
