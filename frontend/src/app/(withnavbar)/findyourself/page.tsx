550c21ab81f9f8f13a520cccf24506b584c8a192
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-03-17 21:48:26 +0700

    FindYourSelf JS UI

--- frontend/src/app/(withnavbar)/findyourself/page.tsx
+++ frontend/src/app/(withnavbar)/findyourself/page.tsx
@@ -7,22 +7,32 @@ import Button1 from '@/components/button/Button1'
 import ChoiceQuiz from '@/components/button/ChoiceQuiz'
 
 import Image from 'next/image'
-import { useState } from 'react' 
+import { useEffect, useState } from 'react' 
 
 export default function FindYourSelf () {
     const questionList = Q['question_list']
     const questionCount = questionList.length
 
+    let buttonStatus = [false, false, false, false]
+
     const [questionIndex, setQuestionIndex] = useState(1)
     const [questionAnswer, setQuestionAnswer] = useState(new Array(questionCount).fill(null))
     
     const genAnswer  = (answer : string) => {
-        let newAnswer = questionAnswer
+        let newAnswer = [...questionAnswer]
         newAnswer[questionIndex - 1] = answer
         return newAnswer
     }
+
     const indexNow = questionIndex - 1
     const thisQuestionObject = questionList[indexNow]
+
+    for (let i = 0 ; i < 4 ; i++) {
+        buttonStatus[i] = thisQuestionObject['choices'][i]['field'] == questionAnswer[questionIndex - 1]
+        console.log(buttonStatus[i])
+    }
+    
+
     return (
         <div className={styles.wrapper}>
             <Image
@@ -48,10 +58,10 @@ export default function FindYourSelf () {
                 <h2 className='mt-[16px]'>ข้อ {questionIndex} / {questionCount}</h2>
                 <h1 className='mt-[16px]'>{thisQuestionObject['question']}</h1>
             <div className={styles['choice-wrapper']}>
-                <ChoiceQuiz onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][0]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][0]['text']}></ChoiceQuiz>
-                <ChoiceQuiz onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][1]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][1]['text']}></ChoiceQuiz>
-                <ChoiceQuiz onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][2]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][2]['text']}></ChoiceQuiz>
-                <ChoiceQuiz onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][3]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][3]['text']}></ChoiceQuiz>
+                <ChoiceQuiz active = {buttonStatus[0]} onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][0]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][0]['text']}></ChoiceQuiz>
+                <ChoiceQuiz active = {buttonStatus[1]} onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][1]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][1]['text']}></ChoiceQuiz>
+                <ChoiceQuiz active = {buttonStatus[2]} onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][2]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][2]['text']}></ChoiceQuiz>
+                <ChoiceQuiz active = {buttonStatus[3]} onClick={() => setQuestionAnswer(genAnswer(thisQuestionObject['choices'][3]['field']))} mt={24} size={24} text = {thisQuestionObject['choices'][3]['text']}></ChoiceQuiz>
                 
                 {(questionIndex != questionCount && questionIndex == 1)  &&
                     <div className={styles['op-wrapper']}>
--- frontend/src/components/button/ChoiceQuiz.tsx
+++ frontend/src/components/button/ChoiceQuiz.tsx
@@ -1,32 +1,37 @@
 export default function ChoiceQuiz({ 
     size = 16,
     text = "ตัวหนังสือ",
+    active = false,
     mt = 0,
     onClick
 }: {
     size?: number;
     text?: string;
+    active? : boolean;
     mt? : number;
-    onClick? : Function
+    onClick : Function
 }){
     return(
-        <button className="
-            w-[100%]
-            bg-white hover:bg-subsecondary active:bg-highlight1
+        <button className={`
+            w-full
+            active:bg-highlight1
+            hover:bg-subsecondary
+            ${active ? "bg-highlight1 border-white" : "bg-white"}
+            ${active ? "border-white" : "border-secondary"}
             text-black
             font-regular 
             py-2 px-8 
             rounded-2xl
             border-2
-            border-secondary active:border-white
+            active:border-white
             shadow-[-2px_-2px_10px_rgba(250,250,250,0.5),4px_4px_10px_rgba(62,92,152,0.3)]
             transition-all duration-300 ease-in-out
             group
             items-center
             justify-center
-            "
-            style={{ fontSize: `${size}px` , marginTop: `${mt}px`}}
-            onClick={onClick}
+            `}
+            style={{ fontSize: `${size}px`, marginTop : `${mt}px`}}
+            onClick={() => onClick()}
         >
             {text}
         </button>
