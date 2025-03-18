e73c1bf855582f2b7c16e01153c3669ca74ba688
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-03-19 01:29:20 +0700

    123

--- frontend/src/app/(withnavbar)/yan/page.tsx
+++ frontend/src/app/(withnavbar)/yan/page.tsx
@@ -10,13 +10,14 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import YanSelection from '@/components/button/YanSelection/YanSelection'
 import { useRouter } from 'next/router'
-import { useParams } from 'next/navigation'
+import { redirect, useParams } from 'next/navigation'
 // import { useEffect, useState } from 'react' 
 
 export default function Yan () {
 
     const [allYanImage, setAllYanImage] = useState({success : false, data :[[]]})
     const [stateImage, setStateImage] = useState(new Array(4).fill(null))
+    const [backgroundColor, setBackgroundColor] = useState('#112141')
 
     useEffect(() => {
         const x = async () => {
@@ -28,7 +29,6 @@ export default function Yan () {
         
     },[])
 
-    
     const handleClick = (mode : string , layer : number) => {
         let newStateImage = [...stateImage]
         let nowStateImageThisLayer = newStateImage[layer]
@@ -46,7 +46,6 @@ export default function Yan () {
                 newStateImage[layer] = null
             else newStateImage[layer] = newStateImage[layer] - 1
         }
-        console.log(newStateImage)
         setStateImage(newStateImage)
 
     }
@@ -73,10 +72,34 @@ export default function Yan () {
             else
                 str += 'null'
 
-            if (i != 3)
                 str += '-'
         }
+        str += backgroundColor.slice(1)
         return str
+    }
+
+    const genID = () => {
+        let str = ''
+        for (let i = 0 ; i < 4 ; i++) {
+            if (stateImage[i] != null)
+                str += ( allYanImage['data'][i][stateImage[i]]['yan_template_image_set_id'])
+            else
+                str += 'null'
+
+                str += '-'
+        }
+        str += backgroundColor.slice(1)
+        return str
+    }
+
+    const handleTextInputChange = (e : any) => {
+        let txt = e.target.value 
+        const s = new Option().style;
+        s.color = txt;
+        if (s.color !== '')
+            setBackgroundColor(txt)
+        else
+            return
     }
     return (
         <div className={styles['wrapper']}>
@@ -96,21 +119,13 @@ export default function Yan () {
                     <div className={styles['left-side-wrapper']}>
                         <div className={styles['yan-scope']}>
                             <div className={styles['yan-boarder']}>
-                                <div id='yan-background' className={styles['yan-background']}>
-                                    {/* <div style={{backgroundColor : 'aqua'}} className={styles['yan-inner-first']}>
-                                        <div style={{backgroundColor : 'black'}} className={styles['yan-inner'] + " z-2"}>
-                                            <div style={{backgroundColor : 'green'}} className={styles['yan-inner'] + " z-3"}>
-                                                <div style={{backgroundColor : 'pink'}} className={styles['yan-inner'] + " z-4"}>
-
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div> */}
+                                <div id='yan-background' style={{backgroundColor : `${backgroundColor}`}} className={styles['yan-background']}>
+                                   
                                     {allYanImage['data'][0][stateImage[0]] != null && (
                                     <Image
                                         src={`data:image/jpeg;base64, ${allYanImage['data'][0][stateImage[0]]['yan_image_base64']}`}
                                         alt=""
-                                        layout="intrinsic"
+                                        // layout="intrinsic"
                                         width={10000}
                                         height={10000}
                                         className='absolute w-[364px] z-0 opacity-[100%]'
@@ -120,7 +135,7 @@ export default function Yan () {
                                     <Image
                                         src={`data:image/jpeg;base64, ${allYanImage['data'][1][stateImage[1]]['yan_image_base64']}`}
                                         alt=""
-                                        layout="intrinsic"
+                                        // layout="intrinsic"
                                         width={10000}
                                         height={10000}
                                         className='absolute w-[364px] z-0 opacity-[100%]'
@@ -130,7 +145,7 @@ export default function Yan () {
                                     <Image
                                         src={`data:image/jpeg;base64, ${allYanImage['data'][2][stateImage[2]]['yan_image_base64']}`}
                                         alt=""
-                                        layout="intrinsic"
+                                        // layout="intrinsic"
                                         width={10000}
                                         height={10000}
                                         className='absolute w-[364px] z-0 opacity-[100%]'
@@ -140,7 +155,7 @@ export default function Yan () {
                                     <Image
                                         src={`data:image/jpeg;base64, ${allYanImage['data'][3][stateImage[3]]['yan_image_base64']}`}
                                         alt=""
-                                        layout="intrinsic"
+                                        // layout="intrinsic"
                                         width={10000}
                                         height={10000}
                                         className='absolute w-[364px] z-0 opacity-[100%]'
@@ -151,8 +166,8 @@ export default function Yan () {
                             
                         </div>
                         <div className={styles['bottom-button-wrapper']}>
-                            <Button1 minWidth={'150px'} icon='Download' front={true} text='เสร็จสิ้น'></Button1>
-                            <Button1 onClick={() => {console.log(genURL())}} minWidth={'150px'} icon='Share' text='แชร์'></Button1>
+                            <Button1 onClick={() => {redirect(`/export/` + genID())}} minWidth={'150px'} icon='Download' front={true} text='เสร็จสิ้น'></Button1>
+                            <Button1 onClick={() => {console.log(navigator.clipboard.writeText(genURL()))}} minWidth={'150px'} icon='Share' text='คัดลองลิงก์'></Button1>
                         </div>
                     </div>
 
@@ -162,7 +177,24 @@ export default function Yan () {
                             <YanSelection rightButtonClick={() => {handleClick('inc', 1)}} leftButtonClick={() => {handleClick('dec', 1)}} layer={2} innerText={getInnerText(1)} description={getCategory(1)}></YanSelection>
                             <YanSelection rightButtonClick={() => {handleClick('inc', 2)}} leftButtonClick={() => {handleClick('dec', 2)}} layer={3} innerText={getInnerText(2)} description={getCategory(2)}></YanSelection>
                             <YanSelection rightButtonClick={() => {handleClick('inc', 3)}} leftButtonClick={() => {handleClick('dec', 3)}} layer={4} innerText={getInnerText(3)} description={getCategory(3)}></YanSelection>
-                            <div className={styles['color-wrapper']}></div>
+                            <div className={styles['color-wrapper']}>
+                                <div onClick={() => setBackgroundColor('#EB463C')} style={{backgroundColor : '#EB463C'}} className={styles['circle']}></div>
+                                <div onClick={() => setBackgroundColor('#E07CAE')} style={{backgroundColor : '#E07CAE'}} className={styles['circle']}></div>
+                                <div onClick={() => setBackgroundColor('#8D4BF6')} style={{backgroundColor : '#8D4BF6'}} className={styles['circle']}></div>
+                                
+                                <div className='flex items-center justify-center'>
+                                    <input onChange={(e) => setBackgroundColor(e.target.value)} style={{opacity : 0, backgroundColor : 'transparent', zIndex : 10, width : '48px', height : '48px'}} type="color" name="" id="" />
+                                    <Image
+                                        src='/dropper.png'
+                                        width={10000}
+                                        height={10000}
+                                        alt=''
+                                        style={{position : 'absolute', height : '48px', width : '48px'}}
+                                    />
+                                </div>
+                                
+                                <input onChange={(e) => {handleTextInputChange(e)}} type="text" placeholder='#FFFFFF' maxLength={7}/>
+                            </div>
                         </div>
                     </div>
                 </div>
